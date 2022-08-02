d43d9a1dc162c9a7f65d29cb1f1dda1b9ccc74d1
Faiz <faizalam2002@gmail.com>
2022-08-02 19:32:44 +0530

    mNFT

--- MNFT.sol
+++ MNFT.sol
@@ -8,14 +8,20 @@ import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721Burnable.sol
 import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
 
-contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
+contract mNFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
-
     Counters.Counter private _tokenIdCounter;
-
-    constructor() ERC721("MyToken", "MTK") {}
-
-    function safeMint(address to, string memory uri) public onlyOwner {
+    
+    address minter;
+    constructor() ERC721("MyToken", "MTK" ) {
+        minter= msg.sender;
+    }
+    
+    function setMinter(address _minter) public onlyOwner{
+        minter=_minter;
+    }
+    function safeMint(address to, string memory uri) public  {
+        require(msg.sender== minter ,"only minter can mint");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
